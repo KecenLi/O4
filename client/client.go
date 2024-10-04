3c4220609b061cf93f8fde1cf0dc24092edf5bb0
KecenLi <yo22285@bristol.ac.uk>
2024-10-04 17:37:53 +0100

    Completed Task

--- client/client.go
+++ client/client.go
@@ -1,24 +1,45 @@
-package main
+package main // 定义包名为 main，表示这是一个可独立运行的程序
 
 import (
-	"flag"
-	"net"
-	"fmt"
+	"bufio" // 缓冲读写，提供了带缓冲的 I/O 操作
+	"flag"  // 命令行参数解析
+	"fmt"   // 格式化 I/O，提供打印输出功能
+	"net"   // 网络相关功能，提供 TCP/IP 网络支持
+	"os"    // 操作系统相关功能，提供文件和输入输出接口
 )
 
+// 从服务器读取消息的函数，持续运行
 func read(conn net.Conn) {
-	//TODO In a continuous loop, read a message from the server and display it.
+	reader := bufio.NewReader(conn) // 创建一个新的读取器，用于从服务器连接中读取数据
+	for {
+		message, _ := reader.ReadString('\n') // 读取一行数据，直到遇到换行符 '\n'，忽略错误
+		fmt.Print("\rReceived: ", message)    // 打印从服务器收到的消息，使用 '\r' 覆盖当前行
+		fmt.Print("Enter message: ")          // 提示用户输入消息
+	}
 }
 
+// 向服务器发送消息的函数，持续运行
 func write(conn net.Conn) {
-	//TODO Continually get input from the user and send messages to the server.
+	reader := bufio.NewReader(os.Stdin) // 创建一个新的读取器，用于从标准输入（键盘）读取用户输入
+	for {
+		fmt.Print("Enter message: ")       // 提示用户输入消息
+		text, _ := reader.ReadString('\n') // 读取用户输入的一行数据，直到遇到换行符 '\n'，忽略错误
+		conn.Write([]byte(text))           // 将用户输入的消息转换为字节数组，发送给服务器，忽略错误
+	}
 }
 
 func main() {
-	// Get the server address and port from the commandline arguments.
+	// 定义一个命令行参数，用于指定服务器的 IP 地址和端口，默认值为 "127.0.0.1:8030"
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
-	flag.Parse()
-	//TODO Try to connect to the server
-	//TODO Start asynchronously reading and displaying messages
-	//TODO Start getting and sending user messages.
+	flag.Parse() // 解析命令行参数
+
+	conn, _ := net.Dial("tcp", *addrPtr)            // 尝试建立到服务器的 TCP 连接，忽略错误
+	defer conn.Close()                              // 在程序结束时关闭连接
+	fmt.Println("Connected to server at", *addrPtr) // 打印连接成功的信息
+
+	// 异步读取和显示来自服务器的消息
+	go read(conn) // 启动一个新的 Goroutine，执行 read 函数，读取服务器发送的消息
+
+	// 开始获取和发送用户输入的消息
+	write(conn) // 调用 write 函数，读取用户输入并发送给服务器
 }
--- server/server.go
+++ server/server.go
@@ -1,63 +1,72 @@
-package main
+package main // 定义包名为 main，表示这是一个可独立运行的程序
 
 import (
-	"bufio"
-	"flag"
-	"net"
-	"fmt"
+	"bufio" // 缓冲读写，提供了带缓冲的 I/O 操作
+	"flag"  // 命令行参数解析
+	"fmt"   // 格式化 I/O，提供打印输出功能
+	"net"   // 网络相关功能，提供 TCP/IP 网络支持
 )
 
+// 定义一个消息结构体，用于存储发送者 ID 和消息内容
 type Message struct {
-	sender  int
-	message string
+	sender  int    // 发送者的客户端 ID
+	message string // 消息内容
 }
 
-func handleError(err error) {
-	// TODO: all
-	// Deal with an error event.
-}
+var clients = make(map[int]net.Conn) // 创建一个映射，用于存储客户端 ID 和对应的连接
 
+// 接受新的客户端连接的函数，持续运行
 func acceptConns(ln net.Listener, conns chan net.Conn) {
-	// TODO: all
-	// Continuously accept a network connection from the Listener
-	// and add it to the channel for handling connections.
+	for {
+		conn, _ := ln.Accept() // 等待并接受新的客户端连接，忽略错误
+		conns <- conn          // 将新的连接发送到通道 conns
+	}
 }
 
+// 处理客户端消息的函数，为每个客户端启动一个协程
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
-	// TODO: all
-	// So long as this connection is alive:
-	// Read in new messages as delimited by '\n's
-	// Tidy up each message and add it to the messages channel,
-	// recording which client it came from.
+	reader := bufio.NewReader(client) // 创建一个新的读取器，从客户端连接中读取数据
+	for {
+		message, _ := reader.ReadString('\n') // 从客户端读取一行数据，直到遇到换行符 '\n'，忽略错误
+		// 创建一个新的消息结构体，包含发送者 ID 和消息内容
+		msg := Message{
+			sender:  clientid,
+			message: message,
+		}
+		msgs <- msg // 将消息发送到通道 msgs，供服务器主循环处理
+	}
 }
 
 func main() {
-	// Read in the network port we should listen on, from the commandline argument.
-	// Default to port 8030
+	// 定义一个命令行参数，用于指定服务器监听的端口，默认值为 ":8030"
 	portPtr := flag.String("port", ":8030", "port to listen on")
-	flag.Parse()
+	flag.Parse() // 解析命令行参数
 
-	//TODO Create a Listener for TCP connections on the port given above.
+	ln, _ := net.Listen("tcp", *portPtr)                 // 创建一个 TCP 监听器，监听指定的端口，忽略错误
+	defer ln.Close()                                     // 在程序结束时关闭监听器
+	fmt.Println("Server is listening on port", *portPtr) // 打印服务器启动信息
 
-	//Create a channel for connections
-	conns := make(chan net.Conn)
-	//Create a channel for messages
-	msgs := make(chan Message)
-	//Create a mapping of IDs to connections
-	clients := make(map[int]net.Conn)
+	conns := make(chan net.Conn) // 创建一个通道，用于存储新的客户端连接
+	msgs := make(chan Message)   // 创建一个通道，用于存储收到的消息
+	var clientID int = 0         // 初始化客户端 ID
 
-	//Start accepting connections
-	go acceptConns(ln, conns)
+	go acceptConns(ln, conns) // 启动一个协程，持续接受新的客户端连接
 	for {
 		select {
-		case conn := <-conns:
-			//TODO Deal with a new connection
-			// - assign a client ID
-			// - add the client to the clients map
-			// - start to asynchronously handle messages from this client
-		case msg := <-msgs:
-			//TODO Deal with a new message
-			// Send the message to all clients that aren't the sender
+		case conn := <-conns: // 如果有新的客户端连接
+			clientID++                                       // 增加客户端 ID
+			id := clientID                                   // 当前客户端的 ID
+			clients[id] = conn                               // 将新的客户端连接存储到映射中
+			fmt.Println("New client connected with ID:", id) // 打印新客户端连接信息
+			go handleClient(conn, id, msgs)                  // 启动一个协程，处理该客户端的消息
+		case msg := <-msgs: // 如果收到新的消息
+			fmt.Printf("Message received from client %d: %s", msg.sender, msg.message) // 打印收到的消息
+			// 将消息发送给除发送者之外的所有客户端
+			for id, conn := range clients {
+				if id != msg.sender {
+					conn.Write([]byte(msg.message)) // 将消息发送给其他客户端，忽略错误
+				}
+			}
 		}
 	}
 }
